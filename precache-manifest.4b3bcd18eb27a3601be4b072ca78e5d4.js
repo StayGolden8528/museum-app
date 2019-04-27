e6e88cf6d2fd29c3f2b90535b68473448c0ecefd
StayGolden8528 <bobby.brice@gmail.com>
2019-04-27 14:41:16 -0400

    Updates

---
+++ precache-manifest.4b3bcd18eb27a3601be4b072ca78e5d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca134c613a9f2d0e3d6cb697eaa32124",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "70fb4143951d527b3486",
+    "url": "/museum-app/static/css/main.de3f1410.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum-app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "70fb4143951d527b3486",
+    "url": "/museum-app/static/js/main.25b783d1.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
